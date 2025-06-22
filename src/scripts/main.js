94714563a2e22f5d2db3af5dcd498a39911732d8
Savtenko Volodymyr <volodimer.sovtenko2018@gmail.com>
2025-06-22 20:16:44 +0300

    add solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,20 @@
 'use strict';
+
+const thumbs = document.querySelector('#thumbs');
+const largeImg = document.querySelector('#largeImg');
+
+thumbs.addEventListener('click', (e) => {
+  e.preventDefault();
+
+  if (e.target.tagName === 'IMG') {
+    const href = e.target.parentElement.href;
+
+    largeImg.src = href;
+  }
+
+  if (e.target.tagName === 'A') {
+    const href = e.target.href;
+
+    largeImg.src = href;
+  }
+});
